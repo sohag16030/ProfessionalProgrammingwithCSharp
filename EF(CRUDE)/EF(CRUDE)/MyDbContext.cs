1eb77012785ae8dcf78483d71c232c588b6fede7
shohag_shadaf <shohag.mbstu.it16030@gmail.com>
2020-02-15 10:18:55 +0600

    why this problem

---
+++ EF(CRUDE)/EF(CRUDE)/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_CRUDE_
+{
+    class MyDbContext :DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer("Server=DESKTOP-VKV44NQ;Database=ABCLibrary;Integrated Security=True");
+        }
+    }
+}
+
---
+++ EF(CRUDE)/EF(CRUDE)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EF_CRUDE_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new MyDbContext();
+            Book newbook = new Book("B213","jango","pabble",54);
+            db.Add(newbook);
+            db.SaveChanges();
+
+        }
+    }
+}
